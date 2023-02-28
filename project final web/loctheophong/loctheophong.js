75e1b0626aae092330f00413b63fcccb78c095b2
ThanhVuong2002 <121165387+ThanhVuong2002@users.noreply.github.com>
2023-02-28 16:37:57 +0500

    finished

---
+++ project final web/loctheophong/loctheophong.js
@@ -0,0 +1,346 @@
+var phongtieuchuangiuongdon=[
+    {
+        image:'https://huyhoanhotel.com/wp-content/uploads/2017/05/1-giuong-doi-1-giuong-don.jpg',
+        id:"1",
+        name:"Phòng giường đôi 1",
+        code:"231BT32",
+        price:"2.000.000 đ"
+    },
+    {
+        image:'https://parishotelbinhthuan.com/uploads/h%C3%ACnh%20m%E1%BB%9Bi/khachsantaibautrang1.jpg',
+        id:"2",
+        name:"Phòng giường đôi 2",
+        code:"24BT563",
+        price:"2.300.000 đ"
+    },
+    {
+        image:'https://vinapad.com/wp-content/uploads/2019/04/phong-doi-la-gi-1.jpg',
+        id:"3",
+        name:"Phòng giường đôi 3",
+        code:"34AS3423",
+        price:"1.500.000 đ"
+    },
+    {
+        image:'https://phuquocbluehotel.com/wp-content/uploads/2017/12/15-1000x667.jpg',
+        id:"4",
+        name:"Phòng giường đôi 4",
+        code:"23SDF232",
+        price:"2.300.000 đ"
+    },
+    {
+        image:'https://khachsanph558.com/wp-content/uploads/2018/07/TUA03746-scaled.jpg',
+        id:"1",
+        name:"Phòng giường đôi 5",
+        code:"231BT32",
+        price:"1.500.000 đ"
+    },
+    {
+        image:'https://timviec365.vn/pictures/images/phong-doi-la-gi.jpg',
+        id:"2",
+        name:"Phòng giường đôi 6",
+        code:"24BT563",
+        price:"2.500.000 đ"
+    },
+    {
+        image:'https://noithatmyhouse.net/wp-content/uploads/2019/11/phong-doi_14.jpg',
+        id:"3",
+        name:"Phòng giường đôi 7",
+        code:"34AS3423",
+        price:"2.100.000 đ"
+    },
+    {
+        image:'https://huyhoanhotel.com/wp-content/uploads/2017/05/p-1-giuong-doi.jpg',
+        id:"4",
+        name:"Phòng giường đôi 8",
+        code:"23SDF232",
+        price:"1.140.000 đ"
+    },
+    {
+        image:'https://noithatduongdai.cdn.vccloud.vn/wp-content/uploads/2022/03/phong-ngu-2-giuong.jpg',
+        id:"1",
+        name:"Phòng giường đôi 9",
+        code:"231BT32",
+        price:"1.500.000 đ"
+    },
+    {
+        image:'https://pix10.agoda.net/hotelImages/agoda-homes/18859868/b99f4bb4605668a392280fa2463552b4.jpg?ca=14&ce=1&s=1024x768',
+        id:"2",
+        name:"Phòng giường đôi 10",
+        code:"24BT563",
+        price:"2.500.000 đ"
+    },
+    {
+        image:'https://pullmanphuquoc.com/wp-content/uploads/sites/225/2020/06/2006_Deluxe-Pool-Side-Twin_2-1-e1592820190882.jpg',
+        id:"3",
+        name:"Phòng giường đôi 11",
+        code:"34AS3423",
+        price:"2.100.000 đ"
+    },
+    {
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROu1IEIQSptMOBjl4c_-2kuqe4EP6oHUSCjw&usqp=CAU',
+        id:"4",
+        name:"Phòng giường đôi 12",
+        code:"23SDF232",
+        price:"1.140.000 đ"
+    }
+    
+]
+
+var phongdondexuve=[
+    {
+        image:'https://cdn.cet.edu.vn/wp-content/uploads/2018/01/phong-deluxe-la-gi.jpg',
+        id:"1",
+        name:"Phòng Đơn Deluxe 1 ",
+        code:"231BT32",
+        price:"1.600.000 đ"
+    },
+    {
+        image:'https://acihotel.vn/wp-content/uploads/2020/07/phong_deluxe_voi-khong-gian-tot.jpg',
+        id:"2",
+        name:"Phòng Đơn Deluxe 2",
+        code:"24BT563",
+        price:"2.300.000 đ"
+    },
+    {
+        image:'https://vinapad.com/wp-content/uploads/2019/03/phong-deluxe-1.jpg',
+        id:"3",
+        name:"Phòng Đơn Deluxe 3",
+        code:"34AS3423",
+        price:"1.000.000 đ"
+    },
+    {
+        image:'https://d2e5ushqwiltxm.cloudfront.net/wp-content/uploads/sites/172/2019/03/01044828/P_Chiffon818_4-edit.jpg',
+        id:"4",
+        name:"Phòng Đơn Deluxe 4",
+        code:"23SDF232",
+        price:"1.400.000 đ"
+    },
+    {
+        image:'https://huyhoanhotel.com/wp-content/uploads/2017/05/1-giuong-doi-1-giuong-don.jpg',
+        id:"1",
+        name:"Phòng Đơn Deluxe 5",
+        code:"231BT32",
+        price:"1.600.000 đ"
+    },
+    {
+        image:'https://vinapad.com/wp-content/uploads/2019/03/phong-deluxe-2.jpg',
+        id:"2",
+        name:"Phòng Đơn Deluxe 6",
+        code:"24BT563",
+        price:"2.300.000 đ"
+    },
+    {
+        image:'https://product.hstatic.net/200000233553/product/170936780_b94d1d81e7e84a5d84062ef66d9f4532.jpg',
+        id:"3",
+        name:"Phòng Đơn Deluxe 7",
+        code:"34AS3423",
+        price:"1.000.000 đ"
+    },
+    {
+        image:'https://kksapahotel.com/uploads/files/Chon%20KS%20sapa%20gia%20re/VQK_2153.jpg',
+        id:"4",
+        name:"Phòng Đơn Deluxe 8",
+        code:"23SDF232",
+        price:"1.400.000 đ"
+    },
+    {
+        image:'https://noithatduongdai.cdn.vccloud.vn/wp-content/uploads/2019/10/tu-van-noi-that-chung-cu-2-phong-ngu-chi-phi-200-trieu-phong-cach-hien-dai-7.jpg',
+        id:"1",
+        name:"Phòng Đơn Deluxe 9",
+        code:"231BT32",
+        price:"1.600.000 đ"
+    },
+    {
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Bs6wPZ-dtb3ogUggESrKc14lqimtlud5ug&usqp=CAU',
+        id:"2",
+        name:"Phòng Đơn Deluxe 10",
+        code:"24BT563",
+        price:"2.300.000 đ"
+    },
+    {
+        image:'https://product.hstatic.net/200000233553/product/170936780_b94d1d81e7e84a5d84062ef66d9f4532.jpg',
+        id:"3",
+        name:"Phòng Đơn Deluxe 11",
+        code:"34AS3423",
+        price:"1.000.000 đ"
+    },
+    {
+        image:'https://kksapahotel.com/uploads/files/Chon%20KS%20sapa%20gia%20re/VQK_2153.jpg',
+        id:"4",
+        name:"Phòng Đơn Deluxe 12",
+        code:"23SDF232",
+        price:"1.400.000 đ"
+    }
+    
+]
+var chungcuhainguoingu=[
+    {
+        image:'https://diamondcrownhaiphong.info/uploads/46ed5b07-fa82-47ba-81a9-e3653f298ce9.png',
+        id:"1",
+        name:"Chung cư hai phòng ngủ 1",
+        code:"231BT32",
+        price:"1.600.000"
+    },
+    {
+        image:'https://bizweb.dktcdn.net/100/399/247/products/noi-that-phong-ngu-bo-me1.jpg?v=1600676352287',
+        id:"2",
+        name:"Chung cư hai phòng ngủ 2",
+        code:"24BT563",
+        price:"2.300.000"
+    },
+    {
+        image:'https://arkitec.vn/wp-content/uploads/2020/05/thiet-ke-can-ho-chung-cu-45m2-hai-phong-ngu-khong-gian-mo-1.jpg',
+        id:"3",
+        name:"Chung cư hai phòng ngủ 3",
+        code:"34AS3423",
+        price:"1.000.000 "
+    },
+    {
+        image:'https://acado.vn/wp-content/uploads/2020/02/thiet-ke-phong-ngu-chung-cu-1.jpg',
+        id:"4",
+        name:"Chung cư hai phòng ngủ 4",
+        code:"23SDF232",
+        price:"1.400.000"
+    },
+    {
+        image:'https://huyhoanhotel.com/wp-content/uploads/2017/05/1-giuong-doi-1-giuong-don.jpg',
+        id:"1",
+        name:"Chung cư hai phòng ngủ 5",
+        code:"231BT32",
+        price:"1.600.000"
+    },
+    {
+        image:'https://parishotelbinhthuan.com/uploads/h%C3%ACnh%20m%E1%BB%9Bi/khachsantaibautrang1.jpg',
+        id:"2",
+        name:"Chung cư hai phòng ngủ 6",
+        code:"24BT563",
+        price:"2.300.000"
+    },
+    {
+        image:'http://noithatchungcu24h.com/wp-content/uploads/2016/07/thiet-ke-noi-that-chung-cu-2-phong-ngu-3.jpg',
+        id:"3",
+        name:"Chung cư hai phòng ngủ 7",
+        code:"34AS3423",
+        price:"1.000.000"
+    },
+    {
+        image:'https://noithatdongca.vn/wp-content/uploads/2018/03/Gi%C6%B0%E1%BB%9Dng-gi%E1%BA%ADt-c%E1%BA%A5p-ph%C3%B2ng-ng%E1%BB%A7.jpg',
+        id:"4",
+        name:"Chung cư hai phòng ngủ 8",
+        code:"23SDF232",
+        price:"1.400.000"
+    },
+    {
+        image:'https://huyhoanhotel.com/wp-content/uploads/2017/05/1-giuong-doi-1-giuong-don.jpg',
+        id:"1",
+        name:"Chung cư hai phòng ngủ 9",
+        code:"231BT32",
+        price:"1.600.000"
+    },
+    {
+        image:'https://parishotelbinhthuan.com/uploads/h%C3%ACnh%20m%E1%BB%9Bi/khachsantaibautrang1.jpg',
+        id:"2",
+        name:"Chung cư hai phòng ngủ 10",
+        code:"24BT563",
+        price:"2.300.000"
+    },
+    {
+        image:'http://noithatchungcu24h.com/wp-content/uploads/2016/07/thiet-ke-noi-that-chung-cu-2-phong-ngu-3.jpg',
+        id:"3",
+        name:"Chung cư hai phòng ngủ 11",
+        code:"34AS3423",
+        price:"1.000.000"
+    },
+    {
+        image:'https://noithatdongca.vn/wp-content/uploads/2018/03/Gi%C6%B0%E1%BB%9Dng-gi%E1%BA%ADt-c%E1%BA%A5p-ph%C3%B2ng-ng%E1%BB%A7.jpg',
+        id:"4",
+        name:"Chung cư hai phòng ngủ 12",
+        code:"23SDF232",
+        price:"1.400.000"
+    }
+    
+]
+function listProducts(){
+for (let i=0; i<= phongtieuchuangiuongdon.length-1; i++){
+    var demo= '<div class="col-3">';
+    demo +='<div class="card" style="width:18rem;">';
+    demo +='<img src="'+phongtieuchuangiuongdon[i].image+'"class="card-img-top" style="height:400px;">';
+    demo +='<div class="card-body">';
+    demo +='<h5 class="card-title">'+phongtieuchuangiuongdon[i].name+'</h5>';
+    demo +='<p class="card-text">'+phongtieuchuangiuongdon[i].price+'</p>';
+    demo +='<a href="http://127.0.0.1:5500/older.html"class="btn btn-primary" onclick="oder()">Đặt mua</a>';
+    demo +='</div>';
+    demo +='</div>';
+    demo +='</div>';
+    console.log(demo);
+    document.getElementById("men").innerHTML +=demo;
+}
+for (let i=0; i<= phongdondexuve.length-1; i++){
+    var demo= '<div class="col-3">';
+    demo +='<div class="card" style="width:18rem;">';
+    demo +='<img src="'+phongdondexuve[i].image+'"class="card-img-top" style="height:400px;">';
+    demo +='<div class="card-body">';
+    demo +='<h5 class="card-title">'+phongdondexuve[i].name+'</h5>';
+    demo +='<p class="card-text">'+phongdondexuve[i].price+'</p>';
+    demo +=''
+    demo +='<a href="#"class="btn btn-primary" onclick="oder()">Đặt mua</a>';
+    demo +='</div>';
+    demo +='</div>';
+    demo +='</div>';
+    console.log(demo);
+    document.getElementById("woment").innerHTML +=demo;
+}
+for (let i=0; i<= chungcuhainguoingu.length-1; i++){
+    var demo= '<div class="col-3">';
+    demo +='<div class="card" style="width:18rem;">';
+    demo +='<img src="'+chungcuhainguoingu[i].image+'"class="card-img-top" style="height:400px;">';
+    demo +='<div class="card-body">';
+    demo +='<h5 class="card-title">'+chungcuhainguoingu[i].name+'</h5>';
+    demo +='<p class="card-text">'+chungcuhainguoingu[i].price+'</p>';
+    demo +='<a href="#"class="btn btn-primary" onclick="oder()">Đặt mua</a>';
+    demo +='</div>';
+    demo +='</div>';
+    demo +='</div>';
+    console.log(demo);
+    document.getElementById("mmmm").innerHTML +=demo;
+}
+}
+localStorage.setItem("Man",JSON.stringify(phongtieuchuangiuongdon));
+localStorage.setItem("Woman",JSON.stringify(phongdondexuve));
+localStorage.setItem("mmmm",JSON.stringify(chungcuhainguoingu));
+function search(){
+    
+}
+
+function changeProductList(type, element) //type là tham số truyền vào để có thể hiểu được khi nào nhấn vào cái này cái kia
+//element để có hiểu rằng kaf mình đã lấy ra được đối tượng nào ra .
+{   
+    let tabs =document.getElementsByClassName('tab-item');
+    // duyệt các mảng này
+    for (i=0; i < tabs.length; i++){
+            tabs[i].style.background ='#3F7BFE';
+            // background gán váo background ban đầu
+    }
+
+    element.style.background='#344583'
+
+    document.getElementById(type).style.display ='flex';
+    switch (type) {
+        case 'men':
+            document.getElementById('woment').style.display="none";
+            document.getElementById('mmmm').style.display="none";
+            break;
+        case 'woment':
+            document.getElementById('men').style.display="none";
+            document.getElementById('mmmm').style.display="bnone";
+            break;
+        case 'mmmm':
+            document.getElementById('men').style.display="none";
+            document.getElementById('woment').style.display="none";
+            break;
+        
+    }
+    console.log(tabs);
+    //console.log(type);
+
+}
